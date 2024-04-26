1a3d96e0df0f86f0748a6382df076456592258a9
bakhtin <a@bakhtin.net>
2024-04-26 19:07:05 +0100

    Tune kernel parameters to build a TDX image
    Signed-off-by: bakhtin <a@bakhtin.net>

--- board/flashbots-tee/post-build.sh
+++ board/flashbots-tee/post-build.sh
@@ -5,13 +5,21 @@ set -e
 BOARD_DIR=$(dirname "$0")
 
 # Detect boot strategy, EFI or BIOS
-if [ -d "$BINARIES_DIR/efi-part/" ]; then
-    cp -f "$BOARD_DIR/grub-efi.cfg" "$BINARIES_DIR/efi-part/EFI/BOOT/grub.cfg"
-else
-    cp -f "$BOARD_DIR/grub-bios.cfg" "$TARGET_DIR/boot/grub/grub.cfg"
+#if [ -d "$BINARIES_DIR/efi-part/" ]; then
+#    cp -f "$BOARD_DIR/grub-efi.cfg" "$BINARIES_DIR/efi-part/EFI/BOOT/grub.cfg"
+#else
+#    echo
+#    cp -f "$BOARD_DIR/grub-bios.cfg" "$TARGET_DIR/boot/grub/grub.cfg"
+#
+#    # Copy grub 1st stage to binaries, required for genimage
+#    cp -f "$TARGET_DIR/lib/grub/i386-pc/boot.img" "$BINARIES_DIR"
+#fi
 
-    # Copy grub 1st stage to binaries, required for genimage
-    cp -f "$TARGET_DIR/lib/grub/i386-pc/boot.img" "$BINARIES_DIR"
+# Add a console on tty1
+if [ -e ${TARGET_DIR}/etc/inittab ]; then
+    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
+        sed -i '/GENERIC_SERIAL/a\
+tty1::respawn:/sbin/getty -L  tty1 0 vt100 # QEMU graphical window' ${TARGET_DIR}/etc/inittab
 fi
 
 # Set root password
